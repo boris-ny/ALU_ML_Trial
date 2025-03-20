53f9904c7d5a5f8ce10c892eb5d7053e6847db6e
Afsaumutoniwase <a.umutoniwa@alustudent.com>
2025-03-20 17:17:33 +0200

    adding port compatible with deployment sservice

--- app.py
+++ app.py
@@ -55,5 +55,6 @@ async def predict(data: HydroponicInput):
 
 # Run API (for local development)
 if __name__ == "__main__":    
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=int(os.getenv("PORT", 8000)))
+
 
